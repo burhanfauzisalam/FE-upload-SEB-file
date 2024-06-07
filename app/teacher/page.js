0ce5cd79d98a783faceafcbaf2857d32d9b91853
= <=>
2024-06-07 22:40:57 +0700

    add some features

--- app/teacher/page.js
+++ app/teacher/page.js
@@ -1,6 +1,6 @@
 "use client";
 import FileTable from "../components/fileTable";
-import FileUploadForm from "../components/upload";
+// import FileUploadForm from "../components/upload";
 
 const Dashboard = () => {
   return (
